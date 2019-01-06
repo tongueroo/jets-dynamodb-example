e666c169c8bdd6b7aa1edd60ae20fc8e78648eea
Tung Nguyen <tongueroo@gmail.com>
2019-01-06 17:57:01 +0000

    first commit

---
+++ spec/controllers/posts_controller_spec.rb
@@ -0,0 +1,16 @@
+# Example:
+# describe PostsController, type: :controller do
+#   it "index returns a success response" do
+#     event = payload("posts-index")
+#     controller = PostsController.new(event)
+#     response = controller.index
+#     # pp response
+#   end
+
+#   it "show returns a success response" do
+#     event = payload("posts-show")
+#     controller = PostsController.new(event)
+#     response = controller.show
+#     # pp response
+#   end
+# end

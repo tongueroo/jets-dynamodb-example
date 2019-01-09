aaf60e31def6ef1f4687b3c7d143aa077f365fc6
Tung Nguyen <tongueroo@gmail.com>
2019-01-09 15:14:54 +0000

    add comment

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -3,6 +3,8 @@ ENV["JETS_ENV"] ||= "test"
 # Ensures aws api never called. Fixture home folder does not contain ~/.aws/credentails
 ENV['HOME'] = "spec/fixtures/home"
 
+# Work around dynamodb-local assumpion that AWS_ACCESS_KEY_ID is set
+# https://community.rubyonjets.com/t/running-rspec-over-dynomite-model-with-local-dynamodb-instance-results-in-missingcredentialserror/31/5
 if ENV['DYNAMODB_TEST_AWS_SECRET_ACCESS_KEY']
   ENV['AWS_SECRET_ACCESS_KEY'] = ENV['DYNAMODB_TEST_AWS_SECRET_ACCESS_KEY']
 end

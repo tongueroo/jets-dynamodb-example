0df73b8b67aa0da45b546db3638050b024132039
Tung Nguyen <tongueroo@gmail.com>
2019-01-09 15:11:29 +0000

    work around dynamodb-local assumption that AWS_ACCESS_KEY_ID is required

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -3,6 +3,14 @@ ENV["JETS_ENV"] ||= "test"
 # Ensures aws api never called. Fixture home folder does not contain ~/.aws/credentails
 ENV['HOME'] = "spec/fixtures/home"
 
+if ENV['DYNAMODB_TEST_AWS_SECRET_ACCESS_KEY']
+  ENV['AWS_SECRET_ACCESS_KEY'] = ENV['DYNAMODB_TEST_AWS_SECRET_ACCESS_KEY']
+end
+if ENV['DYNAMODB_TEST_AWS_ACCESS_KEY_ID']
+  ENV['AWS_ACCESS_KEY_ID'] = ENV['DYNAMODB_TEST_AWS_ACCESS_KEY_ID']
+end
+ENV['AWS_REGION'] ||= 'us-west-2'
+
 require "byebug"
 require "fileutils"
 require "jets"
